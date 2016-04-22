559a646b569917c0a527018f41f5c5345f5b5790
darshandsoni <darshan@alumni.ubc.ca>
2016-04-22 15:06:15 -0700

    More edits to it

--- create.py
+++ create.py
@@ -1,12 +1,32 @@
 import time
 today = time.strftime("%Y-%m-%d")
 print "The date is: " + today
+
+custom_date = raw_input('Would you prefer a custom date (y/n)? ')
+if custom_date == 'n':
+    print('Today\'s date chosen')
+    today_month = time.strftime(" %b ")
+    today_day = time.strftime("%d")
+elif custom_date == 'y':
+    today = raw_input('Enter custom date e.g. 2016-12-02: ')
+else:
+    print('That is not a valid answer!')
+
 category = input("Enter 1 for President's update: ")
 if category == 1:
     print "Category is: President's Update"
-    posttitle = "Presidents-Update"
+    filename_main = "Presidents-Update"
+    category = "president-update"
+    author = "Gabriel Lessard"
+    image = "/images/truss.png"
 else:
     print "Category is: General post"
 
-title = today + "-" + posttitle + ".md"
-print "Title is: " + title
+filename = today + "-" + filename_main + ".md"
+print "File name is: " + filename
+
+yaml_layout = "layout: post"
+yaml_title = "title: " + raw_input('Enter title here with no special chars: ') + today_month + today_day
+yaml_categories = "categories: " + category
+yaml_author = "author: " + author
+yaml_image = "image: " + image
